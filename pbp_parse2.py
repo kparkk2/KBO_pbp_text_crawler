3d1f3ce6693ce51fb339e5cb1a1d755486543cb5
kparkk2 <eliath2@naver.com>
2018-01-21 01:06:25 +0900

    parser2

--- pbp_parse2.py
+++ pbp_parse2.py
@@ -5,38 +5,11 @@ import json
 from collections import OrderedDict
 import regex
 
-pos_string = ["투수", "포수", "1루수", "2루수", "3루수",
-              "유격수", "좌익수", "중견수", "우익수",
-              "지명타자", "대타", "대주자"]
-
-bb_dir_string = ["투수", "포수", "1루수", "2루수", "3루수",
-                 "유격수", "좌익수", "중견수", "우익수",
-                 "우중간", "좌중간"]
-
-res_string = ["1루타", "안타" "2루타", "3루타", "홈런", "실책", "땅볼",
-              "뜬공", "희생플라이", "희생번트", "야수선택", "삼진",
-              "낫아웃", "타격방해", "직선타", "병살타", "번트", "내야안타"]
-
-hit_results = ['안타', '1루타', '2루타', '3루타', '홈런']
-
-out_results = ['땅볼', '플라이', '병살타', '라인드라이브',
-               ' 번트', '희생번트', '삼중살']
-
-nobb_outs = ['삼진', '낫 아웃', '타구맞음', '쓰리번트', '부정타격']
-
-runner_outs = ['견제사', '  포스아웃', '  태그아웃',
-               '도루실패', '진루실패', '터치아웃', '공과']
-
-pass_games = ['20130609HHSK', '20130611LGHH', '20170509KTHT', '20130619LGNC',
-              '20130912HTLG', '20140829LGSK', '20141017LGLT', '20150426LGNC',
-              '20150505LGOB', '20150612LGHH', '20120513LTHH', '20120601HHLG',
-              '20120603HHLG', '20120607LGWO', '20120613SKLG', '20120621LGHH',
-              '20120630LGSK', '20120719SKLG', '20120725LGOB']
-
 # OOO : XXX
 # exception : '타석이탈위반'(이름), 'BH'
 # caution : mind searching priority!
 # 다음 text 받기 전, 타자주자 설정 함
+
 pa_results = ['1루타',
               '2루타',
               '3루타',
@@ -208,6 +181,11 @@ class BallGame:
         'pa_result': None,
         'pitch_result': None,
 
+        # base
+        'runner_1b': None,
+        'runner_2b': None,
+        'runner_3b': None,
+
         # pfx data
         'pitch_type': None,
         'speed': None,
@@ -219,11 +197,6 @@ class BallGame:
         'release_z': None,
         'sz_bot': None,
         'sz_top': None,
-
-        # base
-        'runner_1b': None,
-        'runner_2b': None,
-        'runner_3b': None,
 
         # home & away
         'home': None,
@@ -275,6 +248,27 @@ class BallGame:
 
     def print_row(self):
         print('\r')
+
+    def print_row_debug(self):
+        # for debug
+        row = 'Date:' + self.game_status['game_date'] + ', ' +\
+              'P:' + self.game_status['pitcher'] + ', ' +\
+              'B:' + self.game_status['batter'] + ', ' +\
+              'BSO:' + self.game_status['balls'] + '/' +\
+              self.game_status['strikes'] + '/' +\
+              self.game_status['outs'] + ', ' +\
+              'Inn:' + self.game_status['inning']
+        if self.game_status['inning_topbot'] == 0:
+            row += '초, '
+        else:
+            row += '말, '
+        row += 'Score:' + self.game_status['score_away'] + ':' +\
+               self.game_status['score_home'] + ', ' +\
+               'Base:' + self.game_status['runner_1b'] + '/' +\
+               self.game_status['runner_2b'] + '/' +\
+               self.game_status['runner_3b'] + ', ' +\
+               'Event:' + self.game_status['pa_result'] + ', ' +\
+               'Pitch:' + self.game_status['pitch_result'] + ', '
 
     def set_home_away(self, home, away):
         self.game_status['home'] = home
@@ -348,23 +342,17 @@ class BallGame:
         cur_balls = self.game_status['balls']
         if cur_balls == 4:
             self.game_status['pa_result'] = '볼넷'
-        self.print_row()
+        # self.print_row()
+        self.print_row_debug()
         self.game_status['balls'] += 1
-        '''
-        cur_balls = self.game_status['balls']
-        if cur_balls == 4:
-            self.get_ball_four()
-            self.go_to_next_pa()
-        else:
-            self.print_row()
-        '''
 
     def get_strike(self):
         self.game_status['pitch_result'] = '스트라이크'
         cur_strikes = self.game_status['strikes']
         if cur_strikes == 3:
             self.game_status['pa_result'] = '삼진'
-        self.print_row()
+        # self.print_row()
+        self.print_row_debug()
         self.game_status['strikes'] += 1
 
     def get_swing_miss(self):
@@ -372,7 +360,8 @@ class BallGame:
         cur_strikes = self.game_status['strikes']
         if cur_strikes == 3:
             self.game_status['pa_result'] = '삼진'
-        self.print_row()
+        # self.print_row()
+        self.print_row_debug()
         self.game_status['strikes'] += 1
 
     def get_bunt_swing_miss(self):
@@ -380,24 +369,27 @@ class BallGame:
         cur_strikes = self.game_status['strikes']
         if cur_strikes == 3:
             self.game_status['pa_result'] = '삼진'
-        self.print_row()
+        # self.print_row()
+        self.print_row_debug()
         self.game_status['strikes'] += 1
 
     def get_foul(self):
         self.game_status['pitch_result'] = '파울'
-        self.print_row()
+        # self.print_row()
+        self.print_row_debug()
         if self.game_status['strikes'] < 2:
             self.game_status['strikes'] += 1
 
     def get_bunt_foul(self):
         self.game_status['pitch_result'] = '번트파울'
-        self.print_row()
+        # self.print_row()
+        self.print_row_debug()
         if self.game_status['strikes'] < 2:
             self.game_status['strikes'] += 1
 
     def get_in_play(self):
         self.game_status['pitch_result'] = '타격'
-        self.print_row()
+        # self.print_row()
 
     # --------------------------------
     # pa result
